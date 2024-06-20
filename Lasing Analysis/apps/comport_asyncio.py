bf7a82721cf22eebbea2aff37174cb6bdd4f1a27
atzphere <allen10to11@gmail.com>
2024-06-20 15:06:31 +0200

    actually added data and new version of comport-asyncio

---
+++ Lasing Analysis/apps/comport_asyncio.py
@@ -0,0 +1,175 @@
+import asyncio
+import serial_asyncio
+import serial
+import logging
+import time
+from typing import Tuple
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.WARNING)
+
+DEFAULT_EOL_CHAR = b'\r' # programs such as PIX connect need this
+DEBUG = False
+
+'''
+TODO: make query handle ports that aren't actually functional
+'''
+
+class COMInterfaceException(Exception):
+    pass
+
+class IncompleteResponseException(Exception):
+    pass
+
+class NoResponseException(IncompleteResponseException):
+    pass
+
+class QueryFailedException(Exception):
+    pass
+
+class COMInterface(object):
+
+    def __init__(self, com_port):
+        self.com_port = com_port
+        self.io_lock = asyncio.Lock()
+
+    async def query_port(self, queries, **kwargs):
+        '''
+        Attempts to query the port for information, wait a certain amount,
+        then read the port for information.
+
+        This is ASYNCHRONOUS: it must be run with through asyncio, i.e.
+        asyncio.run(), asyncio.gather(). This allows OptrisHandler to process
+        many requests concurrently without blocking.
+
+        param queries: str or List[str]: The query string(s), sent one by one.
+                                         Do not include EOL characters here.
+
+        '''
+        try:
+            async with self.io_lock:
+                responses = await query_port(self.com_port, queries, **kwargs)
+        except IncompleteResponseException as e:
+            raise
+        except Exception as e:
+            raise COMInterfaceException("COM Query failed") from e
+        return responses
+
+async def query_port(port, commands, tolerant=False, **kwargs):
+    '''
+    Queries a given COM port with one or more commands
+
+    params:
+
+    port: str : The port to connect and interact with.
+
+    commands: str or List[Str]: The query(s) to send to the port. Order is respected.
+
+    tolerant: bool default False: Whether or not to raise an exception upon a dropped response.
+                                  Returns false on failure if true. Use to handle faults externally
+
+    kwargs passed to other functions: use_optris_workaround: use to deal with Pix Connect dropping every 3rd request.
+    
+    returns: str or List[str]: list of responses from the serial device.
+
+    '''
+    query_start = time.time()
+    timed_out = False
+    reader, writer = await open_com_connection(port, **kwargs)
+    logging.debug(f"opening COM connection took {time.time() - query_start} seconds.")
+    logging.debug(f"Port {port} initialized.")
+    try:
+        start = time.time()
+        messages = commands
+        if type(messages) == str:
+            message_count = 1
+        else:
+            message_count = len(messages)
+
+        await send_commands(writer, messages, **kwargs)
+        logging.debug(f"sending took {time.time() - start} seconds.")
+
+        start = time.time()
+        try:
+            received = await read(reader, message_count, tolerant=tolerant, **kwargs)
+            logging.debug(f"receiving took {time.time() - start} seconds.")
+        except (IncompleteResponseException, NoResponseException) as e: # error catching flag
+            timed_out = True
+            received = False
+
+    finally: # try to shutdown the writer cleanly
+        writer.close()
+        start = time.time()
+        if timed_out and tolerant:
+            logging.warn("One or more queries timed out")
+        elif timed_out and not tolerant:
+            raise QueryFailedException("Query failed to get valid response")
+        else:
+            await writer.wait_closed() # this hangs if there was a timeout during the query
+            logging.debug(f"closing took {time.time() - start} seconds.")
+            logging.debug(f"Port {port} closed.")
+        logging.info(f"Query took {time.time() - query_start} seconds. ({(time.time() - query_start) / message_count:.4f}s per request)")
+    return received
+
+
+async def open_com_connection(port, baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
+    '''
+    Initializes a COM connection, returns StreamReader and StreamWriter objects
+    '''
+    reader, writer = await serial_asyncio.open_serial_connection(url=port, baudrate=baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
+    return reader, writer
+
+
+async def send_commands(w, msgs, delay=0.04, eol_character=DEFAULT_EOL_CHAR, optris_workaround=True):
+    if type(msgs) == str:
+        cmd = msgs.encode() + DEFAULT_EOL_CHAR
+        w.write(cmd)
+        logging.debug(f'sent: {cmd.decode().rstrip()}')
+        await asyncio.sleep(delay)
+    else:
+        for count, msg in enumerate(msgs):
+            cmd = msg.encode() + DEFAULT_EOL_CHAR
+            w.write(cmd)
+            logging.debug(f'sent: {cmd.decode().rstrip()}')
+            if count == 2 and optris_workaround:
+                await asyncio.sleep(delay * 1.25) # works better with optris which likes to otherwise miss the 3rd message
+            else:
+                await asyncio.sleep(delay)
+            await w.drain()
+    logging.debug('Done sending')
+
+
+async def read(r, expected_msgs=1, readline_eol_character=DEFAULT_EOL_CHAR, tolerant=False, optris_workaround=True, timeout=0.1):
+    messages = []
+    logging.debug(f"Expecting {expected_msgs} messages.")
+    for i in range(expected_msgs):
+        try:
+            if i == 2 and optris_workaround:
+                msg = await asyncio.wait_for(r.readuntil(readline_eol_character), timeout=0.01)
+            else:
+                msg = await asyncio.wait_for(r.readuntil(readline_eol_character), timeout=timeout)
+            logging.debug(f'received: {msg}')
+            messages.append(msg)
+        except asyncio.exceptions.TimeoutError as e:
+            if len(messages) == expected_msgs - 1 and expected_msgs > 1 and optris_workaround:
+                logging.debug(f'one message out of {len(messages)} timed out, janking')
+                messages.insert(2, "timed out")
+                break
+            elif tolerant and len(messages) > 0:
+                logging.warn(f'one message out of {len(messages)} timed out')
+                messages.append("timed out")
+            elif len(messages) > 0:
+                raise IncompleteResponseException(f"Reading serial port timed out: Not all queries were returned.") from e
+            else:
+                raise NoResponseException("Reading serial port timed out: No response received.") from e
+    logging.debug("Done receiving")
+
+    if len(messages) == 1:
+        return messages[0]
+    else:
+        return messages
+
+
+if __name__ == "__main__":
+    logging.debug(asyncio.run(query_port(
+        "COM4", ['?T', '!ImgTemp', '?AreaLoc(0)', 'qux'])))
---
+++ Lasing Analysis/apps/optris_csv.py
@@ -0,0 +1,117 @@
+# Optris Temperature vs. time reader for arbitrary number of ROIs. Useful for testing.
+# Use OptrisDataset(filepath).
+
+import csv
+import numpy as np
+import re
+import time
+
+
+def timestr_to_seconds(time_str) -> float:
+    # HHours:MMinutes:SSeconds.Miliseconds
+    units = [3600, 60, 1, 1E-3]
+    working_string = time_str
+    components = working_string.split(":")
+    components += components[2].split(".")
+    del components[2]
+    time_seconds = sum(
+        [unit * int(measure) for unit, measure in zip(units, components)])
+    return time_seconds
+
+
+def get_profile_and_index(name):
+    separated = re.split(r'\s+(?=\d)|(?<=\d)\s+', name)
+    profile_name = separated[0] + " " + separated[1][0]
+    index = int(separated[1][1:])
+    return profile_name, index
+
+
+def is_profile(name):
+    return "profile" in name
+
+
+class DictDataset:
+    def __init__(self):
+        self.writeabledata = {}
+
+    def organize(self, name, value, rownum):
+        if is_profile(name):  # handles 1D temperature data
+            # get relative position of the profile point and the profile's name
+            profile_name, index = get_profile_and_index(name)
+
+            if profile_name in self.writeabledata.keys():
+                # add a new empty row to array if needed
+                if rownum + 1 > len(self.writeabledata[profile_name]):
+                    self.writeabledata[profile_name].append([0])
+                data_row = self.writeabledata[profile_name][rownum]
+                length_diff = (index + 1) - len(data_row)
+                if length_diff > 0:  # expand length of row appropriately
+                    data_row += [0] * length_diff
+                data_row[index] = value  # update value in row
+                # update data with new row
+                self.writeabledata[profile_name][rownum] = data_row
+            else:
+                data_row = [0] * (index + 1)
+                data_row[index] = value
+                self.writeabledata.update({profile_name: [data_row]})
+        else:
+            if name in self.writeabledata.keys():
+                self.writeabledata[name].append(value)
+            else:
+                self.writeabledata.update({name: [value]})
+
+    def build_array_data(self):
+        self.array_data = {}
+        for key in self.writeabledata.keys():
+            self.array_data.update({key: np.array(self.writeabledata[key])})
+        return self.array_data
+
+
+class TemperatureProfile:
+    # A
+    def __init__(self):
+        pass
+
+
+class OptrisDataset(DictDataset):
+    def __init__(self, filepath):  # auto builds dataaset
+        super().__init__()
+        self.filepath = filepath
+        self.times = []
+        self.profiles = []
+        with open(filepath) as f:
+            reader = csv.reader(f)
+
+            for i in range(6):  # blow past useless header data
+                next(reader)
+            series_headers = next(reader)[1:-1]  # extract ROI names
+
+            for row_num, row in enumerate(reader):  # per row
+                try:  # extract values for time + each ROI
+                    time = timestr_to_seconds('.'.join(row[:2]))
+                    self.organize("time", time, row_num)
+                    for num, series in enumerate(series_headers):
+                        working_column = 2 * num + 2
+                        value_components = [
+                            int(i) for i in row[working_column:working_column + 2]]
+                        value = value_components[0] + value_components[1] / 10
+
+                        self.organize(series, value, row_num)
+                except IndexError:
+                    pass  # horrible but deals with end of file strings
+
+    def slice_by_time(self, label, start, stop, closed=True):
+        '''
+        Returns a time slice of a specified subdataset
+        '''
+        if self.array_data is None:
+            self.build_array_data()
+
+        if closed:
+            return self.array_data[label][np.logical_and(start <= self.array_data["time"], self.array_data["time"] <= stop)]
+        else:
+            return self.array_data[label][np.logical_and(start < self.array_data["time"], self.array_data["time"] < stop)]
+
+
+if __name__ == "__main__":
+    pass
