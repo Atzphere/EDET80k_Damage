b83779b5e927fd8f1812ae98325052234e92846f
Allen Zhao <allen10to11@gmail.com>
2024-07-04 17:11:12 +0200

    stable albeit buggy and divergent simulator

--- Lasing Analysis/apps/thermal sim/run_simulation.py
+++ Lasing Analysis/apps/thermal sim/run_simulation.py
@@ -3,16 +3,21 @@ import lasinglib as ll
 import numpy as np
 
 SILICON = sl.Material(80, 0.09, 0.7, 0.002329002)
-CHIP = sl.SimGrid(30, 101, 0.03, use_spar=True,
+CHIP = sl.SimGrid(30, 101, 0.03, use_spar=False,
                   spar_thickness=0.5, spar_width=1)
 
 sim = sl.Simulation(CHIP, SILICON, 7, pulses=None, ambient_temp=300,
-                    starting_temp=3000, neumann_bc=True,
+                    starting_temp=300, neumann_bc=True,
                     edge_derivative=0, sample_framerate=24, intended_pbs=1,
-                    dense_logging=False, timestep_multi=1)
+                    dense_logging=False, timestep_multi=0.5)
 
 CENTER = (CHIP.CENTERPOINT)
 
-# p = ll.LaserPulse()
+pulses = []
+a = ll.LaserPulse(CHIP, 0, 1, (15, 15), 50, sigma=0.3)
+pulses.append(a)
+
+sim.pulses = pulses
+
 
 sim.simulate()
