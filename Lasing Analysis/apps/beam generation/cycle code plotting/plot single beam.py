d3aa906bcd4bd7807f2ef25d4bbc3af40abbbeb0
Allen Zhao <allen10to11@gmail.com>
2024-07-12 16:02:56 +0200

    cycle code beam current plotter for visualization

---
+++ Lasing Analysis/apps/beam generation/cycle code plotting/plot single beam.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+DPATH = "./gaussian profile 1 2.5A 60S 3H.csv"
+
+data = pd.read_csv(DPATH, header=None)
+
+time = np.cumsum(data[2]) - data[2][0]
+voltage = data[3]
+
+print(data)
+plt.plot(time, voltage)
+plt.show()
