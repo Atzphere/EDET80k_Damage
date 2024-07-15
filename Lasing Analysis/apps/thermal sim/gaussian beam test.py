7610df5f172649d4bb9c9b12bc2c4a27dc740467
Allen Zhao <allen10to11@gmail.com>
2024-07-15 13:49:39 +0200

    modulator library and multiprocessed testing

---
+++ Lasing Analysis/apps/thermal sim/gaussian beam test.py
@@ -0,0 +1,55 @@
+import simulationlib as sl
+import lasinglib as ll
+import measurelib as ml
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import logging
+import multiprocess as mp
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.WARNING)
+
+SILICON = sl.Material(88, 0.09, 0.7, 0.002329002)
+CHIP = sl.SimGrid(dimension=30, resolution=91, thickness=0.03, use_spar=False,
+                  spar_thickness=0.5, spar_width=1)
+
+CENTER = (CHIP.CENTERPOINT)
+
+pulses = []
+
+DURATION = 12
+START_TIME = 0
+
+
+def trial(p):
+    p_string = f"PULSE_{START_TIME}s+{DURATION}s_{p}A"
+    data_ind = p_string + " MEAN"
+    time_ind = p_string + " time"
+    a = ll.LaserPulse(CHIP, START_TIME, DURATION, CHIP.CENTERPOINT,
+                      p, sigma=0.2, measure_target=True, measure_timestep=0.025)
+    sim = sl.Simulation(CHIP, SILICON, duration=DURATION, pulses=[a], ambient_temp=300,
+                        starting_temp=300, neumann_bc=True,
+                        edge_derivative=0, sample_framerate=0, intended_pbs=1,
+                        dense_logging=False, timestep_multi=1, radiation=True, progress_bar=False, silent=True)
+    sim.simulate()
+
+    data = sim.recorded_data
+    return data[time_ind], data[data_ind]
+
+
+if __name__ == "__main__":
+    power = np.arange(0, 2, 0.1)
+    with mp.Pool(mp.cpu_count()) as pol:
+        dat = pol.map(trial, power)
+
+    color = iter(cm.rainbow(np.linspace(0, 1, len(power))))
+    for d, p in zip(dat, power):
+        c = next(color)
+        T, A = d
+        plt.plot(T, A - 273.15, label=f"{p:.2f}A", c=c)
+
+
+    plt.legend()
+    plt.show()
---
+++ Lasing Analysis/apps/thermal sim/modulators.py
@@ -0,0 +1,83 @@
+import numpy as np
+import math
+
+
+def make_exp_pulse(up_time, hold_time, power_rampup):
+    duration = up_time + hold_time
+
+    def function(t):
+        if t <= up_time:
+            val = np.exp(((3 * t) / up_time) - 3) ** power_rampup
+        elif t < up_time + hold_time:
+            val = 1
+        elif t <= duration:
+            val = 0
+        return val
+
+    return function
+
+
+def normal_curve(start_time, sigma):
+    def function(t):
+        return (1 / (sigma * math.sqrt(2 * np.pi))) * np.exp((-1 / 2) * ((t - start_time)**2 / sigma**2))
+
+    return function
+
+
+def doubleSinePulse(up_time, hold_time, down_time, power_rampup=1, power_rampdown=1):
+    duration = up_time + down_time + hold_time
+
+    def function(t):
+        if t <= up_time:
+            val = np.sin((np.pi / (2 * up_time)) * t)**power_rampup
+        elif t < up_time + hold_time:
+            val = 1
+        elif t <= duration:
+            val = np.sin((np.pi / (2 * up_time)) * (t - (up_time + hold_time)) + np.pi / 2)**power_rampdown
+        return val
+
+    return function
+
+
+def doubleGaussianRamp(I0, hold_time, sigma, cutoff=3, boost=0,):
+    I_reduced = I0 - boost
+    half_time = hold_time / 2
+
+    print(f"pulse duration: {2 * cutoff * sigma + hold_time:.2f} seconds")
+
+    def gaussian(t):
+        return np.exp((- 1 / 2) * (t**2 / sigma**2))
+
+    def curve_vec(t):
+        y = t.copy()
+        up_filter = t < -(hold_time / 2)
+        down_filter = t > (hold_time / 2)
+        y[up_filter] = I_reduced * gaussian(t[up_filter] + (hold_time / 2)) + boost
+        y[down_filter] = I_reduced * gaussian(t[down_filter] - (hold_time / 2)) + boost
+        y[~np.logical_or(up_filter, down_filter)] = I0
+        return y
+
+    def curve(t):
+        if t < -(hold_time / 2):
+            return I_reduced * gaussian(t + half_time) + boost
+        elif t > (hold_time / 2):
+            return I_reduced * gaussian(t - half_time) + boost
+        else:
+            return I0
+
+    def function(t):
+        return curve(t - hold_time / 2 - cutoff * sigma)
+
+    return function
+
+
+if __name__ == "__main__":
+    j = doubleGaussianRamp(2.5, 4, 2, cutoff=2, boost=0)
+    import matplotlib.pyplot as plt
+    f = np.linspace(0, 12, 900)
+    y = []
+    for v in f:
+        y.append(j(v))
+    plt.plot(f, y)
+    plt.xlim(0)
+    plt.show()
